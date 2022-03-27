efa6ff346b5a3c0e08319bc405df40604b08f9c4
djbobbo <bohous.steier@seznam.cz>
2022-03-28 00:17:10 +0200

    Smenarna Konecne funguje !

--- Smenarna.py
+++ Smenarna.py
@@ -1,14 +1,15 @@
-#!/usr/bin/env python3
-
 from os.path import basename, splitext
+from re import L
 import tkinter as tk
 import random
 from tkinter import  CENTER, E, END, LEFT, TOP,  N, W, UNDERLINE, BOTTOM, HORIZONTAL, Label, Button, Scale,  StringVar, Frame, Entry , Listbox,Radiobutton,LabelFrame
-from tkinter.ttk import Labelframe
-   
 
-# from tkinter import ttk
+        # Základ
 
+class About(tk.Toplevel):
+    def __init__(self, parent):
+        super().__init__(parent, class_=parent.name)
+        self.config()
 
 class Application(tk.Tk):
     name = basename(splitext(basename(__file__.capitalize()))[0])
@@ -17,153 +18,120 @@ class Application(tk.Tk):
     def __init__(self):
         super().__init__(className=self.name)
         self.title(self.name)
-        self.bind("<Escape>", self.quit)
-         
-                #Nadpisek
-        self.lblNazev = tk.Label(self, text="Směnárna")
-        self.lblNazev.grid(row=0, column=1, columnspan=3)
-                #Transakce
-        self.TransakciVariable = tk.IntVar()
-        self.TransakciVariable.set(0)
+        
+        self.lbl = tk.Label(self, text="Směnárna")
+        self.lbl.grid(row=0, column=0)
+        
+        
+        # Labelframe
         
         self.labelframe = tk.LabelFrame(self , text="Transakce")
-        self.labelframe.grid(row=1,column=1, sticky = W)
-                #Nakup a prodej 
-        self.varTransakce = tk.IntVar()
-        self.varTransakce.set(0)
-       
-        self.radiobutton0 =tk.Radiobutton(self.labelframe,variable =self.varTransakce,  text="Nákup", value=0)
-        self.radiobutton0.grid()
-
-        self.radiobutton1 =tk.Radiobutton(self.labelframe, variable =self.varTransakce, text="Prodej", value=1)
+        self.labelframe.grid(row=1,column=0, sticky = W)
+        
+        # Nakup a prodej
+        
+        self.variable = tk.IntVar(self)
+        
+        self.radiobutton1 = Radiobutton(self.labelframe, text="Nákup", variable=self.variable, value=1)
         self.radiobutton1.grid()
+        
+        self.radiobutton2 = Radiobutton(self.labelframe, text="Prodej", variable=self.variable, value=2)
+        self.radiobutton2.grid()
+        self.variable.set(1)
 
-        self.varTransakce.set("Nákup")
-
-                #Mena
-        mena = self.nacteni_listku()  
-
-        self.MenaVar = tk.IntVar()
-        self.MenaVar.set(0)
+        # mena
 
         self.labelframeMen = tk.LabelFrame(self , text="Měna")
-        self.labelframeMen.grid(row=2,column=1,sticky = W)
-        for n,item in enumerate(mena):
-            Radiobutton(self.labelframeMen, text=item[0], variable = self.MenaVar, value = n).grid()
+        self.labelframeMen.grid(row=2,column=0,sticky = W)
         
-            #Kurz 
-        self.kurzVariable =tk.StringVar()
-        self.kurzVariable.set("")
+        self.listbox = tk.Listbox(self.labelframeMen)
+        self.listbox.grid()
+        self.listbox.bind("<ButtonRelease-1>", self.reakce)       
+        
+        # nacteni ze souboru
+        
+        f = open('listek.txt', 'r')
+        slovnik = {}
+        for line in f:
+            self.listbox.insert(tk.END,line.split()[0])
+            slovnik[line.split()[0]] = (line.split()[1:])
 
-        self.mnozstviVariable = tk.StringVar()
-        self.mnozstviVariable.set("")
+        # Kurz
         
         self.labelframe2 = tk.LabelFrame(self , text="Kurz")
-        self.labelframe2.grid(row=3,column=1)
-
-        self.Editmnozstvi0 = tk.Entry (self.labelframe2,state = "readonly" ,textvariable= self.mnozstviVariable )
-        self.Editmnozstvi0.grid()
-
-        self.EditKurz1 = tk.Entry (self.labelframe2,state = "readonly", textvariable= self.kurzVariable)
-        self.EditKurz1.grid()
-            
-
-            #Vypocet
-        
-        self.VstupniVariable = tk.StringVar()
-        self.VstupniVariable.set("")
-        
-        self.VystupniVariable = tk.StringVar()
-        self.VystupniVariable.set("")
-
-        self.labelframe3 = tk.LabelFrame(self , text="Výpočet")
-        self.labelframe3.grid(row=4,column=1)
-        
-        self.EditVstupu2 = tk.Entry (self.labelframe3, textvariable= self.VstupniVariable)
-        self.EditVstupu2.grid()
-
-        self.btn1 = tk.Button (self.labelframe3, text= "Výpočet", command = self.No_sam_bych_to_nevymyslel  ) 
-        self.btn1.grid()
-        
-        self.EditVystupu2 = tk.Entry (self.labelframe3,state = "readonly", textvariable= self.VystupniVariable )
-        self.EditVystupu2.grid()
+        self.labelframe2.grid(row=3,column=0, sticky = W)
         
         
-        #self.listbox = tk.Listbox(self)
-        #self.listbox.grid(row=5 ,column=1 )
-        #for item in [u"", u""]:
-        #    self.listbox.insert(END, item)
-   
-   
+        self.hodnota = tk.StringVar()
+        
+        self.cena = tk.IntVar()
+        self.cenaLbl= tk.Label(self.labelframe2, textvariable=self.cena) 
+        self.cenaLbl.grid()
+        
+        self.hodnotal= tk.Label(self.labelframe2, textvariable= self.hodnota) 
+        self.hodnotal.grid()
+
+        # Vypocet
+        
+        self.labelframe3 = tk.LabelFrame(self , text="Kolik jich chceš")
+        self.labelframe3.grid(row=4,column=0,sticky = W )
+        
+        self.LabelVypln = tk.Label(self.labelframe3)
+        self.LabelVypln.grid()
+        
+        self.mnozstvi = tk.Entry(self.labelframe3)
+        self.mnozstvi.grid()
+        
+        self.LabelVypln2 = tk.Label(self.labelframe3)
+        self.LabelVypln2.grid()
+        
+        self.btn2 = tk.Button(self.labelframe3, text="Výpočet", command=self.vypocet)
+        self.btn2.grid()
+        
+        # Vysledek 
+        
+        self.labelframe4 = tk.LabelFrame(self , text="Výsledek")
+        self.labelframe4.grid(row=5,column=0)
+
+        self.vysledek = tk.IntVar()
+        self.vysledekl= tk.Label(self.labelframe4, textvariable= self.vysledek)
+        self.vysledekl.grid()
+        
+        self.lbl4 = tk.Label(self)
+        self.lbl4.grid()
         
 
+        #Quit
 
-    
-    def nacteni_listku (self):                            #nacitani textu
-        f = open("listek.txt","r")
-        mena = []
-        while 1:
-            radek = f.readline()
-            if radek == "":
-                break 
+        self.bind("<Escape>", self.quit)
+        self.btn1 = tk.Button(self, text="Quit", command = self.quit)
+        self.btn1.grid(row=13, column=0)
 
-            if radek.strip()[0] == "#":
-                continue 
-            mena += [radek.replace(",",".").split()]
-        f.close()
-        for radek in mena:
-                radek[1] = float(radek[1])
-                radek[2] = float(radek[2])
-                radek[1] = float(radek[3])
-        return mena 
+        #funkce
 
-    
-    mnozstvi = 1
-    kurz = 1 
+    def vypocet(self,event=None):
 
-    def No_sam_bych_to_nevymyslel (self):
-        global mnozstvi
-        global kurz 
-        Vstupu = float(self.VstupniVariable.get())
-        Vystupu = kurz/mnozstvi * Vstupu
-        self.VystupniVariable.set(str(Vystupu))
-        
+        a = int(self.mnozstvi.get())
+        b = int(self.cena.get())
+        c = float(self.hodnota.get().replace(",","."))
+        self.vysledekVar = float(a*c/b)
+        self.vysledek.set(self.vysledekVar)
 
-    def Updatekurzu(self, *Args):
-        global mnozstvi
-        global kurz 
-        self.mnozstviVariable.set(str(mena[self.MenaVar.get()][1]))
-        count = mena [self.MenaVar.get()][1]
-        if self.varTransakce.get()==0:
-            self.kurzVariable.set(str(mena[self.MenaVar.get()][2]))
-            kurz = mena [self.MenaVar.get()][2]
+    def reakce(self, event):
+        index = self.listbox.curselection()[0]
+        f = open("listek.txt")
+        self.lines = f.readlines()
+        self.cenaVar = self.lines[index].split()[1]
+        self.cena.set(self.cenaVar)
+        if self.variable.get() == 1: 
+            self.hodnotaVar = self.lines[index].split()[3] 
         else:
-            self.kurzVariable.set(str(mena[self.MenaVar.get()][3]))
-            kurz = mena[self.MenaVar.get()][3]
+            self.hodnotaVar = self.lines[index].split()[2] 
+        self.hodnota.set(self.hodnotaVar)
 
-
-    
-            
-        
-
-        self.TransakciVariable.trace("w",self.Updatekurzu)
-        self.MenaVar.trace("w",self.Updatekurzu)
-        app.bind("<Map>",self.Updatekurzu)
-
-    
-    
-    
-    
     
     def quit(self, event=None):
         super().quit()
-
-   
-    
-
-
-
-
 
 app = Application()
 app.mainloop()
